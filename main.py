6a50777a5de659bb27caba3bfbc67a16e1eae810
maucamsan <maucamsan@gmail.com>
2025-08-31 14:15:02 -0500

    Read relative path to book and add gitignore

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(filepath):
+    contents = ""
+    with open(filepath) as f:
+        contents = f.read()
+    return contents
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
